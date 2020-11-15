6941ae67f5554a74970b095bebe5392ed1c2e9c1
Harold <hahorta@misena.edu.co>
2020-11-15 00:32:44 -0500

    Se modifica la salida del ticket agregando nuevos cambios

--- src/app/core/models/Response/ticket/ResponseTicket.module.ts
+++ src/app/core/models/Response/ticket/ResponseTicket.module.ts
@@ -3,6 +3,9 @@ import { ResponseOrder } from '../order/ResponseOrder.module';
 
 export class ResponseTicket {
   id: string;
-  // order: OrderModule;
+  customer: string;
+  order: ResponseOrder;
   createAt: string;
+  ticketCost: number;
+  ticketCostWithoutIVA: number;
  }
