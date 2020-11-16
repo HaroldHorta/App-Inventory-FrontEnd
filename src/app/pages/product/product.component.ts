f1a7235c91bf73104ca00a0c253a26b58860df24
Harold <hahorta@misena.edu.co>
2020-11-16 15:12:58 -0500

    cambia implementacion para el buscar productos

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -12,8 +12,9 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
 })
 export class ProductComponent implements OnInit {
 
-  originalDataProduct: ResponseProduct[] = new Array();
-  categories: ResponseCategory[] = new Array();
+  products: ResponseProduct[];
+  originalDataProduct: any[];
+
   mainFilter: any;
 
   currentSorting: string;
@@ -31,10 +32,7 @@ export class ProductComponent implements OnInit {
 
 
   ngOnInit(): void {
-
     this.getProductsList();
-    this.getCategoryList();
-
   }
   // metodo para llamar la lista de productos
   getProductsList() {
@@ -43,61 +41,33 @@ export class ProductComponent implements OnInit {
         this.originalDataProduct = product;
         this.mainFilter = {
           search: '',
-          categories: this.categories.slice(0),
         };
-        this.originalDataProduct = this.originalDataProduct.slice(0);
+        this.products = this.originalDataProduct.slice(0);
         this.sortProducts('name');
-      },
-    );
-  }
-
-  getCategoryList() {
-    this.serviceCategory.getCategories().subscribe(
-      categories => {
-        this.categories = categories;
       },
     );
   }
 
   onSearchChange(search) {
     this.mainFilter.search = search.search;
+
     this.updateProducts({
       type: 'search',
       change: search.change,
     });
   }
 
-  onFilterChange(data) {
-    if (data.type === 'category') {
-      if (data.isChecked) {
-        this.mainFilter.categories.push(data.filter);
-      } else {
-        this.mainFilter.categories = this.mainFilter.categories.filter(
-          category => {
-            return category.id !== data.filter.id;
-          });
-      }
-    } else if (data.type === 'custom') {
-      this.mainFilter.customFilter = data.filter;
-    } else if (data.type === 'price') {
-      this.mainFilter.priceFilter = data.filter;
-    }
-    this.updateProducts({
-      type: data.type,
-      change: data.change,
-    });
-  }
-
   updateProducts(filter) {
+
     let productsSource = this.originalDataProduct;
-    const prevProducts = this.originalDataProduct;
+    const prevProducts = this.products;
     let filterAllData = true;
-    if ((filter.type === 'search' && filter.change === 1) || (filter.type === 'category' && filter.change === -1)) {
-      productsSource = this.originalDataProduct;
+    if ((filter.type === 'search' && filter.change === 1)) {
+      productsSource = this.products;
       filterAllData = false;
     }
 
-    this.originalDataProduct = productsSource.filter(product => {
+    this.products = productsSource.filter(product => {
       // Filter by search
       if (filterAllData || filter.type === 'search') {
         if (!product.name.match(new RegExp(this.mainFilter.search, 'i'))) {
@@ -105,43 +75,21 @@ export class ProductComponent implements OnInit {
         }
       }
 
-      // Filter by categories
-      if (filterAllData || filter.type === 'category') {
-        let passCategoryFilter = false;
-        product.category.forEach(product_category => {
-          if (!passCategoryFilter) {
-            passCategoryFilter = this.mainFilter.categories.reduce((found, category) => {
-              return found || product_category === category.id;
-            }, false);
-          }
-        });
-        if (!passCategoryFilter) {
-          return false;
-        }
-      }
       return true;
     });
 
-    // If the number of products increased after the filter has been applied then sort again
-    // If the number of products remained equal, there's a high chance that the items have been reordered.
+   // If the number of products increased after the filter has been applied then sort again
+   // If the number of products remained equal, there's a high chance that the items have been reordered.
     if (prevProducts.length <= this.originalDataProduct.length && this.originalDataProduct.length > 1) {
       this.sortProducts(this.currentSorting);
     }
 
-    // These two types of filters usually add new data to the products showcase so a sort is necessary
-    if (filter.type === 'custom' || filter.type === 'price') {
-      this.sortProducts(this.currentSorting);
-    }
   }
 
   sortProducts(criteria) {
-    this.originalDataProduct.sort((a, b) => {
+    this.products.sort((a, b) => {
       const priceComparison = a.priceSell - b.priceSell;
-      if (criteria === 'priceDes') {
-        return -priceComparison;
-      } else if (criteria === 'priceAsc') {
-        return priceComparison;
-      } else if (criteria === 'name') {
+      if (criteria === 'name') {
         const nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
         if (nameA < nameB) {
           return -1;
