de22455ea4bf5ca8914c9ad9352807eafded54c0
CadenaCristian <cristilopezca@gmail.com>
2020-11-23 10:49:54 -0500

    Se crea el pop up de UpdateUnit

--- src/app/pages/inventory/pop-update-units/pop-update-units.component.ts
+++ src/app/pages/inventory/pop-update-units/pop-update-units.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NbDialogRef } from '@nebular/theme';
+import { RequestUpdateUnit } from '../../../core/models/Request/product/RequestUpdateUnit';
+import { ResponseProduct } from '../../../core/models/Response/product/ResponseProduct.module';
+import { GeneralService } from '../../../core/services/general.service';
+import { InventoryService } from '../../../core/services/inventory.service';
 
 @Component({
   selector: 'ngx-pop-update-units',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopUpdateUnitsComponent implements OnInit {
 
-  constructor() { }
+  units: ResponseProduct;
+  checkOutForm: FormGroup;
+
+  constructor(protected ref: NbDialogRef<PopUpdateUnitsComponent>, 
+    private generalService: GeneralService,
+    private serviceInventory: InventoryService,private formBuilder: FormBuilder) {
+    this.checkOutForm = this.formBuilder.group({
+      id: [''],
+      priceBuy: [''],
+      priceSell: [''],
+      unit: [''],
+    })
+   }
 
   ngOnInit(): void {
+    this.units;
+    console.log("Estas son las unidades",this.units)
   }
 
+  cancel(){
+    this.ref.close();
+  }
+  updateUnit(unit:RequestUpdateUnit){
+    console.log("Este es unit: ", unit);
+    this.serviceInventory.update(unit).subscribe(
+      () => {
+        this.ref.close(unit);
+        const type = 'success';
+        const quote = {title: null, body: 'Unidades actualizadas correctamente'};
+        this.generalService.showToast(type, quote.title, quote.body);
+      },
+    );
+  }
 }
