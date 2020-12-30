739309400b894c2d422d623a6d2427a56bfb0714
hhortasu <harolds1010@gmail.com>
2020-12-30 15:57:38 -0500

    Se crea componente de paginacion

---
+++ src/app/core/services/pagination.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaginationService {
+
+  private paginatorAddedSource = new Subject<any>();
+  private paginatorNumberSource = new Subject<any>();
+  paginatorAdded$ = this.paginatorAddedSource.asObservable();
+  paginatornumber$ = this.paginatorNumberSource.asObservable();
+  pagination: any[] = [];
+  numberPag: any[] = [];
+  constructor() { }
+
+  paginationCount(cantidad) {
+    const numeroPaginas = Math.ceil(cantidad.count / 10);
+    for (var i = 1; i <= numeroPaginas; i++) {
+      this.pagination.push(i);
+    }
+    this.paginatorAddedSource.next(this.pagination);
+  }
+
+  paginationNumber(pag) {
+    this.numberPag = pag;
+    this.paginatorNumberSource.next( this.numberPag );
+  }
+}
